535f122aa8250df3d0355a598182d358d52fbb8e
innes-k <innes.kwak@gmail.com>
2024-01-23 10:15:13 +0900

    주석 정리(미사용 코드 삭제, inputBox 분리 표기)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 import React, { useState } from "react";
 
-// input Box (상단의 제목, 내용, 추가하기) 컴포넌트
+// input Box 컴포넌트 (상단의 제목, 내용, 추가하기)
 function InputBox(props) {
   return (
     <div className="container-inputTitle">
@@ -47,14 +47,7 @@ function InputBox(props) {
 // 부모 컴포넌트 시작
 function App() {
   // box, title, body state설정
-  const [box, setBox] = useState([
-    // { 기본형 box 참고용 주석
-    //   id: 0,
-    //   title: "제목",
-    //   body: "내용",
-    //   isDone: false,
-    // },
-  ]);
+  const [box, setBox] = useState([]);
 
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
@@ -139,43 +132,7 @@ function App() {
       <div className="title">
         <span>☁️ Welcome to To Do List! ⛅️</span>
       </div>
-      {/* input 박스 */}
-      {/* <div className="container-inputTitle">
-        <div className="inputTitle">
-          <div className="form-floating mb-3">
-            <input
-              value={title}
-              onChange={inputTitle}
-              type="email"
-              className="form-control"
-              id="floatingInput"
-              placeholder="inputTitle"
-            />
-            <label htmlFor="floatingInput">제목</label>
-          </div>
-          <div className="form-floating mb-3">
-            <input
-              onKeyPress={onCheckEnter}
-              value={body}
-              onChange={inputBody}
-              type="email"
-              className="form-control"
-              id="floatingInput"
-              placeholder="inputBody"
-            />
-            <label htmlFor="floatingInput">내용을 입력하세요</label>
-          </div>
-          <div className="addBtn">
-            <button
-              onClick={addHandler}
-              type="button"
-              className="btn text-white"
-            >
-              추가하기
-            </button>
-          </div>
-        </div>
-      </div> */}
+      {/* input 박스 - 컴포넌트 분리 */}
       <InputBox
         title={title}
         inputTitle={inputTitle}
